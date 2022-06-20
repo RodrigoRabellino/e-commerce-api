4225bf4246e9e58135af4b4e24d62fa36b101fb0
Rodrigo Rabellino <30850683+RodrigoRabellino@users.noreply.github.com>
2022-06-20 16:10:27 -0300

    json s

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const port = process.env.APP_PORT || 3000;
 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => console.log("listen port: " + port));
