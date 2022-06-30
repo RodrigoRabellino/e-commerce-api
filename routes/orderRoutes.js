0f07b5f8eba5ea4536d55f756e5d6e5f48b6f958
Rodrigo Rabellino <30850683+RodrigoRabellino@users.noreply.github.com>
2022-06-30 14:22:59 -0300

    order routes

---
+++ routes/orderRoutes.js
@@ -0,0 +1,14 @@
+const orderRoutes = require("express").Router();
+const { index, show, store, update, destroy } = require("../controllers/order");
+const isAuthenticated = require("../middlewares/isAuthenticated");
+const isUserAuthenticated = require("../middlewares/isUserAuthenticated");
+const methodOverride = require("method-override");
+orderRoutes.use(methodOverride("_method"));
+
+orderRoutes.get("/", isAuthenticated, index);
+orderRoutes.post("/", isUserAuthenticated, store);
+orderRoutes.put("/", isUserAuthenticated, update);
+// orderRoutes.delete("/", destroy);
+orderRoutes.get("/:id", isUserAuthenticated, show);
+
+module.exports = orderRoutes;
