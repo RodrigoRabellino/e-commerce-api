4aff7c63d4203cb92fdc527b596e08acdbb89d22
1986SebastianSosa <104211986+1986SebastianSosa@users.noreply.github.com>
2022-07-01 13:27:25 -0300

    Products and Category Creation

--- seeders/seederExample.js
+++ seeders/seederExample.js
@@ -31,15 +31,18 @@ const createAdmin = async () => {
 
 const createCategory = async () => {
   const category = await Category.create([
-    { name: "guitar" },
+    { name: "acoustic" },
+    { name: "electric" },
     { name: "amplifier" },
     { name: "accesories" },
+    { name: "bass" },
+    { name: "effects" },
   ]);
   console.log("category created", category);
 };
 
 const createProduct = async () => {
-  for (const guitar of products.guitar) {
+  for (const electric of products.electric) {
     let {
       name,
       description,
@@ -51,7 +54,7 @@ const createProduct = async () => {
       slug,
       show,
       createdBy,
-    } = guitar;
+    } = electric;
     const product = await Product.create({
       name,
       description,
@@ -64,7 +67,34 @@ const createProduct = async () => {
       show,
       createdBy,
     });
-    console.log("guitars created", product);
+    console.log("electric created", product);
+  }
+  for (const acoustic of products.acoustic) {
+    let {
+      name,
+      description,
+      imgUrl,
+      price,
+      stock,
+      categoryId,
+      starred,
+      slug,
+      show,
+      createdBy,
+    } = acoustic;
+    const product = await Product.create({
+      name,
+      description,
+      imgUrl,
+      price,
+      stock,
+      categoryId,
+      starred,
+      slug,
+      show,
+      createdBy,
+    });
+    console.log("acoustic created", product);
   }
   for (const amplifier of products.amplifier) {
     let {
@@ -119,6 +149,60 @@ const createProduct = async () => {
       createdBy,
     });
     console.log("accesories created", product);
+  }
+  for (const bass of products.bass) {
+    let {
+      name,
+      description,
+      imgUrl,
+      price,
+      stock,
+      categoryId,
+      starred,
+      slug,
+      show,
+      createdBy,
+    } = bass;
+    const product = await Product.create({
+      name,
+      description,
+      imgUrl,
+      price,
+      stock,
+      categoryId,
+      starred,
+      slug,
+      show,
+      createdBy,
+    });
+    console.log("basses created", product);
+  }
+  for (const effect of products.effect) {
+    let {
+      name,
+      description,
+      imgUrl,
+      price,
+      stock,
+      categoryId,
+      starred,
+      slug,
+      show,
+      createdBy,
+    } = accesory;
+    const product = await Product.create({
+      name,
+      description,
+      imgUrl,
+      price,
+      stock,
+      categoryId,
+      starred,
+      slug,
+      show,
+      createdBy,
+    });
+    console.log("effects created", product);
   }
 };
 module.exports = { createAdmin, createCategory, createUser, createProduct };
